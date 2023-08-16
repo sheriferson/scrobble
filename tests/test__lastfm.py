598034d40db15f7c300f0f2ab0f03a0d13bb9d2b
Sherif Soliman <code@sherif.io>
2023-08-16 00:11:39 -0700

    More tests.

--- tests/test__lastfm.py
+++ tests/test__lastfm.py
@@ -3,8 +3,8 @@ from scrobble.lastfm import get_lastfm_client
 
 def test_bad_config_path():
     with pytest.raises(FileNotFoundError):
-        lfmclient = get_lastfm_client(session_key_file='/bad/file/path/here.toml')
+        lfmclient = get_lastfm_client(scrobble_config_file='/bad/file/path/here.toml')
 
 def test_bad_config_missing_lastfm_key():
     with pytest.raises(RuntimeError):
-        lfmclient = get_lastfm_client(session_key_file='tests/resources/scrobble_bad_config.toml')
+        lfmclient = get_lastfm_client(scrobble_config_file='tests/resources/scrobble_bad_config.toml')
